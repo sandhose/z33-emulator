5858e138365ba0fdf3181cc36788e3e89d88d655
sandhose <sandhose@users.noreply.github.com>
2021-02-03 23:47:15 +0000

    deploy: 89dc8923abe27a39a21f4764cbc8010a3d6bb284

--- directives-18b9250c.js
+++
@@ -1,2 +0,0 @@
-export default'.string "hello"\n.word 3 + 8 * pmain\npmain: .word main\n.space 5\n\n.addr 100\nmain: reset\n';
-//# sourceMappingURL=directives-18b9250c.js.map
--- handler-5992a27d.js
+++
@@ -1,2 +0,0 @@
-export default"main:\n    trap\n    reset\n\n.addr 200\nhandler:\n    rti\n";
-//# sourceMappingURL=handler-5992a27d.js.map
