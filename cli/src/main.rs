8ed5ad001dc80a29836422d04233535d67387850
Quentin Gliech <quentingliech@gmail.com>
2021-02-23 15:22:20 +0100

    cli: add format flags

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,7 +2,7 @@
 
 use std::process::exit;
 
-use clap::Clap;
+use clap::{ArgGroup, Clap};
 use tracing::error;
 use tracing_subscriber::filter::EnvFilter;
 use tracing_subscriber::prelude::*;
@@ -13,11 +13,23 @@ mod interactive;
 use crate::commands::Subcommand;
 
 #[derive(Clap)]
-#[clap(version, author, about)]
+#[clap(version, author, about, group = ArgGroup::new("format"))]
 struct Opt {
     /// Increase the level of verbosity. Can be used multiple times.
     #[clap(short, long, parse(from_occurrences), global(true))]
     verbose: u8,
+
+    /// Force colored output. Default is to check if the output is a tty
+    #[clap(short = 'c', long, global(true), group = "format")]
+    color: bool,
+
+    /// Force non-colored output. Default is to check if the output is a tty
+    #[clap(short = 'C', long, global(true), group = "format")]
+    no_color: bool,
+
+    /// Use JSON output for log messages
+    #[clap(short, long, global(true), group = "format")]
+    json: bool,
 
     #[clap(subcommand)]
     command: Subcommand,
@@ -27,10 +39,20 @@ impl Opt {
     const fn log_filter(&self) -> &'static str {
         match self.verbose {
             0 => "info",
-            1 => "z33_emulator=debug,z33_cli=debug",
-            2 => "z33_emulator=trace,z33_emulator=trace",
-            3 => "z33_emulator=trace,z33_emulator=trace,debug",
+            1 => "z33_emulator=debug,z33_cli=debug,info",
+            2 => "z33_emulator=trace,z33_cli=trace,info",
+            3 => "z33_emulator=trace,z33_cli=trace,debug",
             4..=u8::MAX => "trace",
+        }
+    }
+
+    fn should_use_colors(&self) -> bool {
+        if self.color {
+            true
+        } else if self.no_color {
+            false
+        } else {
+            atty::is(atty::Stream::Stdout)
         }
     }
 
@@ -48,14 +70,18 @@ fn main() {
     let opt = Opt::parse();
 
     // Then, setup the tracing formatter for logging and instrumentation
-    let fmt_layer = tracing_subscriber::fmt::layer()
-        .without_time()
-        .with_target(false);
+    let registry = tracing_subscriber::Registry::default().with(opt.filter_layer());
 
-    tracing_subscriber::Registry::default()
-        .with(opt.filter_layer())
-        .with(fmt_layer)
-        .init();
+    if opt.json {
+        let json_layer = tracing_subscriber::fmt::layer().json();
+        registry.with(json_layer).init();
+    } else {
+        let fmt_layer = tracing_subscriber::fmt::layer()
+            .without_time()
+            .with_ansi(opt.should_use_colors())
+            .with_target(false);
+        registry.with(fmt_layer).init();
+    }
 
     // And run the command
     let res = opt.command.exec();
