84c587ca7cee860313989e7bd5db1117511a8617
sandhose <sandhose@users.noreply.github.com>
2021-02-28 21:35:19 +0000

    deploy: c1d1f43f53b82547e239513cda449649704eb47c

---
+++ handler-0d0b8f89.js
@@ -0,0 +1,2 @@
+export default"#define EXCEPTION [102]\n#define TRAP 4\n\nmain:\n    trap\n    reset\n\ninvalid:\n    .word 0\n\n.addr 200\nhandler:\n    push %a\n    ld EXCEPTION, %a\n    cmp TRAP, %a\n    jne exit\n    pop %a\n    rti\n\nexit:\n    reset\n";
+//# sourceMappingURL=handler-0d0b8f89.js.map
