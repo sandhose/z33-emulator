4783bb47dd73689051971055ca674f115755eb8a
Quentin Gliech <quentingliech@gmail.com>
2024-05-16 15:15:23 +0200

    Computer render optimisations
    This avoids unnecessary renders in the computer view, by only
    susbcribing to specific values, memoizing components, and splitting the
    sections into their own components

--- web/app/step-form.tsx
+++ web/app/step-form.tsx
@@ -1,3 +1,4 @@
+import React, { startTransition } from "react";
 import { useState, useEffect, useCallback } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -20,127 +21,121 @@ const formSchema = z.object({
 	steps: z.coerce.number().min(1).max(1000).default(1),
 });
 
-export const StepForm: React.FC<{ onStep: () => boolean }> = ({ onStep }) => {
-	const [halt, setHalt] = useState(false);
-	const [panicked, setPanicked] = useState<string | null>(null);
-	const [running, setRunning] = useState(false);
-	const [stepsToRun, setStepsToRun] = useState(0);
-	const [lastStepsValue, setLastStepsValue] = useState(0);
-	const [speed, setSpeed] = useState(1);
+export const StepForm: React.FC<{ onStep: () => boolean }> = React.memo(
+	({ onStep }) => {
+		const [halt, setHalt] = useState(false);
+		const [panicked, setPanicked] = useState<string | null>(null);
+		const [stepsToRun, setStepsToRun] = useState(0);
+		const [lastStepsValue, setLastStepsValue] = useState(0);
+		const [speed, setSpeed] = useState(1);
+		const running = (halt || panicked !== null) && stepsToRun > 0;
 
-	const onStepCallback = useCallback(() => {
-		try {
-			if (onStep?.()) {
-				setHalt(true);
+		const onStepCallback = useCallback(() => {
+			try {
+				if (onStep?.()) {
+					setHalt(true);
+				}
+			} catch (error) {
+				setPanicked(String(error));
 			}
-		} catch (error) {
-			setPanicked(String(error));
+
+			setStepsToRun((prevCounter) => prevCounter - 1);
+		}, [onStep]);
+
+		useEffect(() => {
+			if (running) {
+				const intervalId = setInterval(onStepCallback, 1000 / speed);
+				return () => clearInterval(intervalId);
+			}
+		}, [speed, running, onStepCallback]);
+
+		const form = useForm<z.infer<typeof formSchema>>({
+			resolver: zodResolver(formSchema),
+			defaultValues: { steps: 1, speed: 20 },
+		});
+
+		function onSubmit(values: z.infer<typeof formSchema>) {
+			startTransition(() => {
+				setSpeed(values.speed);
+				setLastStepsValue(values.steps);
+				setStepsToRun(values.steps);
+				onStepCallback();
+			});
 		}
 
-		setStepsToRun((prevCounter) => prevCounter - 1);
-	}, [onStep]);
+		const [currentStepsInput] = form.watch(["steps"]);
+		// For some reason, the input is sometimes a number, sometimes a string
+		const willRunOneStep = `${currentStepsInput}` === "1";
 
-	useEffect(() => {
-		if (halt || panicked !== null) {
-			setRunning(false);
-			return;
-		}
+		return panicked ? (
+			<div className="border p-4 rounded font-bold text-center text-xl border-destructive bg-destructive-foreground text-destructive">
+				Panicked!
+				<div className="text-xs font-bold">{panicked}</div>
+			</div>
+		) : halt ? (
+			<div className="border p-4 rounded font-bold text-center text-xl border-destructive bg-destructive-foreground text-destructive">
+				Halted
+			</div>
+		) : (
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="flex flex-col gap-2 border p-4 rounded"
+				>
+					<FormField
+						control={form.control}
+						name="speed"
+						disabled={stepsToRun > 0}
+						render={({ field }) => (
+							<FormItem className={willRunOneStep ? "hidden" : ""}>
+								<FormLabel>Speed: {field.value} op/s</FormLabel>
+								<FormControl>
+									<Input {...field} min="1" max="100" step="1" type="range" />
+								</FormControl>
+								<FormDescription>How fast to run</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 
-		if (running !== stepsToRun > 0) {
-			setRunning(stepsToRun > 0);
-		}
-	}, [halt, running, panicked, stepsToRun]);
+					<FormField
+						control={form.control}
+						disabled={stepsToRun > 0}
+						name="steps"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Steps</FormLabel>
+								<FormControl>
+									<Input min={1} {...field} type="number" />
+								</FormControl>
+								<FormDescription>How many steps to run</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 
-	useEffect(() => {
-		if (running) {
-			const intervalId = setInterval(onStepCallback, 1000 / speed);
-			return () => clearInterval(intervalId);
-		}
-	}, [speed, running, onStepCallback]);
+					{running ? (
+						<>
+							<Button
+								type="button"
+								variant="destructive"
+								onClick={() => setStepsToRun(0)}
+							>
+								Stop
+							</Button>
 
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: { steps: 1, speed: 20 },
-	});
-
-	function onSubmit(values: z.infer<typeof formSchema>) {
-		setSpeed(values.speed);
-		setLastStepsValue(values.steps);
-		setStepsToRun(values.steps);
-		onStepCallback();
-	}
-
-	const [currentStepsInput] = form.watch(["steps"]);
-	// For some reason, the input is sometimes a number, sometimes a string
-	const willRunOneStep = `${currentStepsInput}` === "1";
-
-	return panicked ? (
-		<div className="border p-4 rounded font-bold text-center text-xl border-destructive bg-destructive-foreground text-destructive">
-			Panicked!
-			<div className="text-xs font-bold">{panicked}</div>
-		</div>
-	) : halt ? (
-		<div className="border p-4 rounded font-bold text-center text-xl border-destructive bg-destructive-foreground text-destructive">
-			Halted
-		</div>
-	) : (
-		<Form {...form}>
-			<form
-				onSubmit={form.handleSubmit(onSubmit)}
-				className="flex flex-col gap-2 border p-4 rounded"
-			>
-				<FormField
-					control={form.control}
-					name="speed"
-					disabled={stepsToRun > 0}
-					render={({ field }) => (
-						<FormItem className={willRunOneStep ? "hidden" : ""}>
-							<FormLabel>Speed: {field.value} op/s</FormLabel>
-							<FormControl>
-								<Input {...field} min="1" max="100" step="1" type="range" />
-							</FormControl>
-							<FormDescription>How fast to run</FormDescription>
-							<FormMessage />
-						</FormItem>
+							{lastStepsValue > 1 ? (
+								<Progress
+									value={(100 * (lastStepsValue - stepsToRun)) / lastStepsValue}
+								/>
+							) : null}
+						</>
+					) : (
+						<Button type="submit">{willRunOneStep ? "Step" : "Run"}</Button>
 					)}
-				/>
-
-				<FormField
-					control={form.control}
-					disabled={stepsToRun > 0}
-					name="steps"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Steps</FormLabel>
-							<FormControl>
-								<Input min={1} {...field} type="number" />
-							</FormControl>
-							<FormDescription>How many steps to run</FormDescription>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-
-				{running ? (
-					<>
-						<Button
-							type="button"
-							variant="destructive"
-							onClick={() => setStepsToRun(0)}
-						>
-							Stop
-						</Button>
-
-						{lastStepsValue > 1 ? (
-							<Progress
-								value={(100 * (lastStepsValue - stepsToRun)) / lastStepsValue}
-							/>
-						) : null}
-					</>
-				) : (
-					<Button type="submit">{willRunOneStep ? "Step" : "Run"}</Button>
-				)}
-			</form>
-		</Form>
-	);
-};
+				</form>
+			</Form>
+		);
+	},
+);
+StepForm.displayName = "StepForm";
